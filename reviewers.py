5af636f7cce453ecf5f0cdfe8cf2e7f529fdcd6d
Bhargav Solanki <bhargavsolanki@bhargavs-macbook-pro.home>
2023-04-08 19:37:49 +0200

    pushed all nodes

--- reviewers.py
+++ reviewers.py
@@ -1,6 +1,12 @@
 import json
 import concurrent.futures
+from neo4j import GraphDatabase
+# Connect to Neo4j database
+uri = "neo4j+s://63e07a57.databases.neo4j.io"
+user = "neo4j"
+password = "8HUdVj-sahaqj8E0n858Ig9vYwNQddckXqejz3MQ_KE"
 
+driver = GraphDatabase.driver(uri, auth=(user, password))
 
 def process_json_line(json_line):
     # Process each JSON line here
@@ -60,12 +66,40 @@ def filter_reviews(filename):
                 if future.exception() is not None:
                     print(f"Exception in thread pool: {future.exception()}")
 
+def insert_nodes():
 
-
+    cypher = "MERGE (r:Reviewer {id: $user_id})" \
+             "MERGE (b:Business {id: $business_id})" \
+             "MERGE (r)-[e:REVIEWED]-(b)" \
+             "SET e.rating = $stars, e.text = $text"
+    count = 0
+    try:
+        with open("filtered.txt", "r") as f:
+            futures = []
+            #with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executer:
+            with driver.session() as session:
+                for line in f:
+                    data = json.loads(line)
+                    print(data)
+                    session.execute_write(lambda tx: tx.run(cypher, **data))
+                    #futures.append(executer.submit(helper, session, data, cypher))
+                    count +=1
+                    #if count >=100:
+                     #break
+    except :
+        print("Count", str(count))
+        #for future in concurrent.futures.as_completed(futures):
+        #    if future.exception() is not None:
+        #        print("Ooops something went wrong")
+def helper(session, data, query):
+    session.execute_write(lambda tx: tx.run(query, **data))
+    return True
 if __name__ == "__main__":
     filename = "yelp_dataset/yelp_academic_dataset_business.json"
     num_threads = 4
     # This filters the file to only businesses in California and new york
     #process_json_file(filename, num_threads)
-    filter_reviews("output.txt")
+    #filter_reviews("output.txt")
+    insert_nodes()
+
 
