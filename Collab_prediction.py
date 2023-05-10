44b445ce96d354bbc3bebb6e4bbe34e17b2b1d16
ZEDz318 <114211455+ZEDz318@users.noreply.github.com>
2023-05-11 00:27:24 +0200

    Update Collab_prediction.py
    This should be working just fine, except for the fact that it reads users that haven't reviewed a business as 0s and adds them in the calculation(doesn't skip 0 values/ will work on it)
    
    We just need to make sure that we can use libraries such as cosine_similarity.... I'm not sure about the matter, but even then it's solvable

--- Collab_prediction.py
+++ Collab_prediction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
+# Read the JSON file
 df = pd.read_json("sub_sample.json", lines=True)
 
 # Create user-item matrix
@@ -11,13 +12,15 @@ user_item_matrix.fillna(0, inplace=True)
 user_similarity = cosine_similarity(user_item_matrix)
 
 # Define target user and item
-target_user_id = 'wSTuiTk-sKNdcFyprzZAjg'
+target_user_id = "OhECKhQEexFypOMY6kypRw"
 target_business_id = 'gebiRewfieSdtt17PTW6Zg'
 
 # Find similar users to the target user
 similar_users_indices = user_similarity[user_item_matrix.index == target_user_id].argsort()[0][-2::-1]
 
-# Predict the target user's rating for the item
-prediction = user_item_matrix.loc[similar_users_indices, target_business_id].mean()
 
-print(f"Predicted rating for user {target_user_id} and item {target_business_id}: {prediction}")
+# Predict the target user's rating for the item
+prediction = user_item_matrix.iloc[similar_users_indices, user_item_matrix.columns == target_business_id].mean()
+#
+print(f'prediction : {prediction}')
+
